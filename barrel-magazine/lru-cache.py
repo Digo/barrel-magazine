5dfa2b0912e398c4cece01d370c1cc212a0902c7
Chuong Ngo <cngo.github@gmail.com>
2017-06-21 11:30:12 -0400

    Added base files.

---
+++ barrel-magazine/lru-cache.py
@@ -0,0 +1,11 @@
+"""
+    A thread-safe LRU Cache implementation.
+"""
+
+import cachetools
+
+class LRUCache:
+    """
+        A thread-safe LRU Cache implementation.
+    """
+    def __init__(ttl=0)
