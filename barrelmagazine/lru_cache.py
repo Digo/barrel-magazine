3079f4c49af0ed2c0ca4d52d65263432c1e07b7e
Chuong Ngo <cngo.github@gmail.com>
2017-06-21 17:27:32 -0400

    Added pop to LRU cache.

--- barrelmagazine/lru_cache.py
+++ barrelmagazine/lru_cache.py
@@ -16,6 +16,17 @@ class LRUCache:
         self._cache = cachetools.TTLCache(maxsize, ttl=ttl) if ttl > 0 else cachetools.LRUCache(maxsize)
         self._lock = threading.Lock()
 
+    def pop(self, key):
+        """
+            Pops the value associated with the key from the cache.
+
+            PARAMETERS:
+
+            key - the key to pop.
+        """
+        with self._lock:
+            return self._cache.pop(key)
+
     def __getitem__(self, key):
         with self._lock:
             return self._cache[key]
