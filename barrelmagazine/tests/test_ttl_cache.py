66e1d2b39c036849c51bdf89fe684b41331a0867
Chuong Ngo <cngo.github@gmail.com>
2017-06-21 17:38:36 -0400

    Updated tests.

--- barrelmagazine/tests/test_ttl_cache.py
+++ barrelmagazine/tests/test_ttl_cache.py
@@ -2,6 +2,8 @@
     Test for LRUCache without a TTL value.
 """
 
+import time
+
 import pytest
 
 import barrelmagazine
@@ -10,7 +12,7 @@ _key = 'test_key'
 
 @pytest.fixture
 def cache():
-    return barrelmagazine.LRUCache()
+    return barrelmagazine.LRUCache(ttl=6000)
 
 def test_invalid_get_value_no_value(cache):
     with pytest.raises(KeyError):
@@ -55,7 +57,7 @@ def test_valid_pop(cache):
         cache[_key]
 
 def test_valid_out_of_space():
-    cache = barrelmagazine.LRUCache(maxsize=1)
+    cache = barrelmagazine.LRUCache(maxsize=1, ttl=6000)
 
     cache['1'] = 1
     assert cache['1'] == 1
@@ -64,3 +66,14 @@ def test_valid_out_of_space():
     assert cache['2'] == 2
     with pytest.raises(KeyError):
         cache['1']
+
+def test_valid_timeout():
+    cache = barrelmagazine.LRUCache(ttl=5)
+
+    cache[_key] = 1
+    assert cache[_key] == 1
+
+    # Wait for timeout
+    time.sleep(7)
+    with pytest.raises(KeyError):
+        cache[_key]
